0094c070c98c08dd7696b384317c899f734941af
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-25 13:20:42 +0300

    Create slice and set up store to manage state

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import listReducer from './list/listSlice';
+
+const store = configureStore({
+  reducer: {
+    list: listReducer,
+  },
+});
+
+export default store;
