6d518807826084b4c9d3e92e0815cdb8c8405418
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-27 13:48:21 +0300

    Add reducers to store for state management

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import listReducer from './list/listSlice';
 import residentsReducer from './location/residentsSlice';
+import detailsReducer from './Details/detailsSlice';
+import charactersReducer from './characters/charactersSlice';
 
 const store = configureStore({
   reducer: {
     list: listReducer,
-    residents: residentsReducer
+    residents: residentsReducer,
+    details: detailsReducer,
+    characters: charactersReducer
   }
 });
 
